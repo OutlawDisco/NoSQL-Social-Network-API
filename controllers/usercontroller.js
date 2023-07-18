91049c106b66ad3d3aa6a343d96f68cd3a7d9a76
OutlawDisco <aoutlaw00@gmail.com>
2023-07-18 09:17:26 -0600

    installed packages and set up folder for controllers, models, and routes

---
+++ controllers/usercontroller.js
@@ -0,0 +1,12 @@
+const { User } = require("../models");
+
+module.exports = {
+  async getAllUsers(req, res) {
+    try {
+      const user = await User.find();
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ routes/api/userroutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const { getAllUsers } = require("../../controllers/usercontroller");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+module.exports = router;
